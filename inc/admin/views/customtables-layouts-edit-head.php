f503512605b551f0d749e37474dfa3820cb41080
JoomlaBoat <ivankomlev@gmail.com>
2023-11-08 20:02:23 -0500

    Version changed

--- inc/admin/views/customtables-layouts-edit-head.php
+++ inc/admin/views/customtables-layouts-edit-head.php
@@ -7,7 +7,7 @@
  */
 function enqueue_codemirror() {
 
-$version = '1.0.1';
+$version = '1.1.0';
 wp_enqueue_style('customtables-js-modal', plugin_dir_url(__FILE__) . '../../../libraries/customtables/media/css/modal.css', false,$version);
 wp_enqueue_style('customtables-js-layouteditor', plugin_dir_url(__FILE__) . '../../../libraries/customtables/media/css/layouteditor.css', false, $version);
 
